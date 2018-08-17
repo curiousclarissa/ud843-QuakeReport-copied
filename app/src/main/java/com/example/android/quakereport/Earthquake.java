a9b4f3e024b3c5bd01369ec240156ffdc3dde73e
curiousclarissa <clarissa.jarem@gmail.com>
2018-08-17 16:36:12 -0700

    needs a color function

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -1,33 +1,78 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.android.quakereport;
 
 /**
- * Created by clarissajarem on 8/16/18.
+ * An {@link Earthquake} object contains information related to a single earthquake.
  */
-
 public class Earthquake {
-    private static String localCity;
-    private static String localMagnitude;
-    private static String localDate;
 
-    /**@param city is where the earthquake took place
-     * @param magnitude is the strength of the earthquake
-     * @param date is when the earthquake happened
+    /** Magnitude of the earthquake */
+    private double localMagnitude;
+
+    /** Location of the earthquake */
+    private String localLocation;
+
+    /** Time of the earthquake */
+    private long localTimeInMilliseconds;
+
+    /** Website URL of the earthquake */
+    private String localUrl;
+
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *
+     * @param magnitude is the magnitude (size) of the earthquake
+     * @param location is the location where the earthquake happened
+     * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
+     *                           earthquake happened
+     * @param url is the website URL to find more details about the earthquake
      */
-    public Earthquake(String city, String magnitude, String date){
-
-        localCity = city;
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
         localMagnitude = magnitude;
-        localDate = date;
-    }
-    public static String getCity() {
-        return localCity;
+        localLocation = location;
+        localTimeInMilliseconds = timeInMilliseconds;
+        localUrl = url;
     }
 
-    public static String getMagnitude() {
+    /**
+     * Returns the magnitude of the earthquake.
+     */
+    public double getMagnitude() {
         return localMagnitude;
     }
 
-    public static int getDate() {
-        return localDate;
+    /**
+     * Returns the location of the earthquake.
+     */
+    public String getLocation() {
+        return localLocation;
+    }
+
+    /**
+     * Returns the time of the earthquake.
+     */
+    public long getTimeInMilliseconds() {
+        return localTimeInMilliseconds;
+    }
+
+    /**
+     * Returns the website URL to find more information about the earthquake.
+     */
+    public String getUrl() {
+        return localUrl;
     }
 }
